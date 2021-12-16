d3b99a4e1408cd23c0d9a47adafaccd4fe6a19e5
wdev733 <wdev733@gmail.com>
2021-12-16 09:08:09 -0500

    allow only rinnkeby

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,11 @@ import { useEffect, useState } from 'react'
 import 'vendor/index.scss'
 import 'vendor/home.scss'
 
+const allowedNetworks = [4]
+
 function App({ Component, router }: AppProps) {
   return (
-    <Layout router={router} networks={[1, 4]}>
+    <Layout router={router} networks={allowedNetworks}>
       <Component />
     </Layout>
   )
