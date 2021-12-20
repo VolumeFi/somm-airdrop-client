e782a64d7cdede0e19dca8a314f11f7409062aff
wdev733 <wdev733@gmail.com>
2021-12-20 13:59:46 -0500

    fix ui issues

--- utils/number.ts
+++ utils/number.ts
@@ -47,3 +47,10 @@ export const getOnlyDigitalValue = (val: number) => {
 export const getOnlyPointsValue = (val: number) => {
   return val - Math.floor(val)
 }
+
+export const numberWithCommas = (x) => {
+  x = x.toString();
+  var pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+  return x;
+}
